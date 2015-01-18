de625f5f210f0959692c48ffeff30cc246347596
falcon170 <johnmccolly@Johns-MacBook-Pro.local>
2015-01-18 00:15:40 -0600

    C++
    Template for Class

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  day 1
+//
+//  Created by John Mccolly on 1/14/15.
+//  Copyright (c) 2015 John Mccolly. All rights reserved.
+//
+
+#include <iostream>
+#include "stdafx.h"
+
+
+
+int main() {
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//   system("pause")
+    return 0;
+}
